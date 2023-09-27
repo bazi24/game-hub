6199d63a95e4f1db6ed579d4b40b3cc8aa4b192e
Sandro <sandrosimmeth@gmail.com>
2023-09-27 19:05:55 +0200

    Small Refactoring

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -25,28 +25,25 @@ const GameGrid = ({
     searchText
   );
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        padding="10px"
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game}></GameCard>
+    <SimpleGrid
+      padding="10px"
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game}></GameCard>
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
