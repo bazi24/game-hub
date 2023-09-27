ba16e58e6528ed9f29d60c00956448337d3d80e0
Sandro <sandrosimmeth@gmail.com>
2023-09-27 12:24:10 +0200

    Implemented the Filter/Fetch by Category

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,18 @@
-import useGenres from "../hooks/useGenres";
-import { Text, HStack, Image, List, ListItem, Spinner } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
 import getCroppedImageURL from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
   return (
@@ -19,7 +29,13 @@ const GenreList = () => {
                 src={getCroppedImageURL(genre.image_background)}
                 borderRadius={8}
               />
-              <Text fontSize="lg">{genre.name}</Text>
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                variant="link"
+                fontSize="lg"
+              >
+                {genre.name}
+              </Button>
             </HStack>
           </ListItem>
         ))}
